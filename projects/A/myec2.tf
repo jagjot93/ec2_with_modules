41951679af5da2f742708c539c77450db0d58eee
jagjot93 <71182607+jagjot93@users.noreply.github.com>
2022-04-29 03:29:32 +0530

    Update myec2.tf

--- projects/A/myec2.tf
+++ projects/A/myec2.tf
@@ -1,5 +1,8 @@
 module "ec2module" {
   source = "../../modules/ec2"
   instancetype = var.instancetype
+  # here, the instance type is written as 'instancetype' and not 'instance_type' because the variable in the module is 'instancetype'
+  # if we do not specify any instance type here, the default instance type of t2.small from the module will be picked up
+  # here the variable 'instancetype' is declared and defined normally using variables.tf and terraform.tfvars
 }
 
